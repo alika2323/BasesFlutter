55cfa373ae09ceefb814b64ec68cc63f3338669e
Nallely Rodriguez <nallely.rf89@gmail.com>
2018-12-12 12:29:02 -0600

    Iniciando proyecto/añadiento ejemplo basico

---
+++ lib/basico.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Tutorial',
+    home: TutorialHome(),
+  ));
+}
+
+class TutorialHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // -- Construyendo AppBar
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: Text('Example title'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+
+      // -- Construyendo Body
+      body: Center(
+        child: Text('Hello, world!'),
+      ),
+
+      // -- Construyendo floatingActionButton
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+    );
+  }
+}
